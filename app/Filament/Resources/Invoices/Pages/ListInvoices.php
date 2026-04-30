a47f0487e9d9ff30214e9fb6529360ef487f3929
abdillah-fh <developer.abdillah@gmail.com>
2026-04-30 18:28:46 +0700

    Add tab filter

--- app/Filament/Resources/Invoices/Pages/ListInvoices.php
+++ app/Filament/Resources/Invoices/Pages/ListInvoices.php
@@ -3,8 +3,11 @@
 namespace App\Filament\Resources\Invoices\Pages;
 
 use App\Filament\Resources\Invoices\InvoiceResource;
+use App\Models\Invoice;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Schemas\Components\Tabs\Tab;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListInvoices extends ListRecords
 {
@@ -14,6 +17,27 @@ class ListInvoices extends ListRecords
     {
         return [
             CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make()
+                ->badge(Invoice::query()->count())
+                ->badgeColor('primary'),
+            'paid' => Tab::make()
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('status', 'paid'))
+                ->badge(Invoice::query()->where('status', 'paid', true)->count())
+                ->badgeColor('success'),
+            'unpaid' => Tab::make()
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('status', 'unpaid'))
+                ->badge(Invoice::query()->where('status', 'unpaid', true)->count())
+                ->badgeColor('danger'),
+            'pending' => Tab::make()
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('status', 'pending'))
+                ->badge(Invoice::query()->where('status', 'pending', true)->count())
+                ->badgeColor('warning'),
         ];
     }
 }
