76d971540f48f81082d995e6ed89e6780cb5ca17
abdillah-fh <developer.abdillah@gmail.com>
2026-05-02 13:13:07 +0700

    Add icon

--- app/Filament/Pages/Settings.php
+++ app/Filament/Pages/Settings.php
@@ -2,9 +2,13 @@
 
 namespace App\Filament\Pages;
 
+use BackedEnum;
 use Filament\Pages\Page;
+use Filament\Support\Icons\Heroicon;
 
 class Settings extends Page
 {
     protected string $view = 'filament.pages.settings';
+
+    protected static string | BackedEnum | null $navigationIcon = Heroicon::OutlinedCog6Tooth;
 }
