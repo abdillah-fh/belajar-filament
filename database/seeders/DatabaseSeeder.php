3aa0b65ec3e84d9de2ffa06a03f89e4049caabaa
abdillah-fh <developer.abdillah@gmail.com>
2026-05-04 00:27:06 +0700

    Edit users field

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,8 +18,10 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Super Admin',
+            'email' => 'superadmin@gmail.com',
+            'password' => bcrypt('superadmin123'),
+            'is_superadmin' => 1
         ]);
     }
 }
